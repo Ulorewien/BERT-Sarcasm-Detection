6d2ef86e0a1202c09b83405ebf1d5926a19d76f1
Ulorewien <rohanshingre7@gmail.com>
2024-05-29 00:10:23 -0700

    Updated dataset

--- datasets.py
+++ datasets.py
@@ -1,13 +1,14 @@
 from torch.utils.data import Dataset
 
 class NewsHeadlinesDataset(Dataset):
-    def __init__(self, features, labels):
+    def __init__(self, features, attention_mask, labels):
         super().__init__()
         self.features = features
+        self.attention_mask = attention_mask
         self.labels = labels
 
     def __len__(self):
         return len(self.features)
     
     def __getitem__(self, index):
-        return self.features[index], self.labels[index]
+        return self.features[index], self.attention_mask[index], self.labels[index]
