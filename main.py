ecdb1095bf3bbe1a49e2bed34ec49cb441cc71f6
Ulorewien <rohanshingre7@gmail.com>
2024-05-29 00:26:39 -0700

    Updated main file and notebook

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import numpy as np
 import pandas as pd
 import transformers
+import torch
+from torch import nn
 from torch.utils.data import DataLoader
 from datasets import NewsHeadlinesDataset
 from model import SarcasmDetectionModel
+from util import train_model, evaluate_model
 
 # Global Variables
+device = "cuda" if torch.cuda.is_available() else "cpu"
 news_dataset_dir = "Datasets\\News_Headlines\\Sarcasm_Headlines_Dataset_v2.json"
 split_ratio = 0.8
 
 # Hyperparameters
 len_dataset = 2000
 batch_size = 32
+lr = 2e-5
+n_epoch = 3
 
 # Load the data
 print("Loading data...")
@@ -38,18 +44,33 @@ padded_dataset_news = np.array([i + [0]*(max_len-len(i)) for i in tokenized_data
 print(f"Shape of the padded dataset: {np.array(padded_dataset_news).shape}")
 
 # Add a mask so that the model doesn't consider padded tokens
-masked_dataset_news = np.where(padded_dataset_news != 0, 1, 0)
-print(f"Shape of the masked dataset: {masked_dataset_news.shape}")
+attention_mask_news = np.where(padded_dataset_news != 0, 1, 0)
+print(f"Shape of the masked dataset: {attention_mask_news.shape}")
 
 # Split the data in training and testing sets and create dataloaders
 split_val = int(split_ratio*len_dataset)
 print(f"Splitting the data -> Train ({split_val}) & Test ({len_dataset-split_val})")
-train_features_news = masked_dataset_news[:split_val]
-test_features_news = masked_dataset_news[split_val:]
-train_labels_news = news_dataset["is_sarcastic"].values[:split_val]
-test_labels_news = news_dataset["is_sarcastic"].values[split_val:]
 
-train_dataset_news = NewsHeadlinesDataset(train_features_news, train_labels_news)
-test_dataset_news = NewsHeadlinesDataset(test_features_news, test_labels_news)
+train_features_news = torch.Tensor(padded_dataset_news[:split_val], device=device)
+train_mask_news = torch.Tensor(attention_mask_news[:split_val], device=device)
+train_labels_news = torch.Tensor(news_dataset["is_sarcastic"].values[:split_val], device=device)
+train_dataset_news = NewsHeadlinesDataset(train_features_news, train_mask_news, train_labels_news)
 train_loader_news = DataLoader(train_dataset_news, batch_size=batch_size, shuffle=True)
+
+test_features_news = torch.Tensor(padded_dataset_news[split_val:], device=device)
+test_mask_news = torch.Tensor(attention_mask_news[split_val:], device=device)
+test_labels_news = torch.Tensor(news_dataset["is_sarcastic"].values[split_val:], device=device)
+test_dataset_news = NewsHeadlinesDataset(test_features_news, test_mask_news, test_labels_news)
 test_loader_news = DataLoader(test_dataset_news, batch_size=batch_size)
+
+# Define the model, optimizer and the loss function
+model = SarcasmDetectionModel(bert_model).to(device)
+optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
+loss_function = nn.CrossEntropyLoss()
+
+# Train and Evaluate the model
+for epoch in range(n_epoch):
+    train_model(model, optimizer, train_loader_news, loss_function)
+    evaluate_model(model, test_loader_news, loss_function)
+
+torch.save(model.state_dict(), "first_run.pth")
